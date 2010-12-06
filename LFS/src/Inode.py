cc4a3ced4c3eefbd2cec5abeb355aa2147e0bcbb
pracheer.gupta@gmail.com <pracheer.gupta@gmail.com@29f8e827-c577-5851-08f9-08740c10fdc4>
2010-12-06 15:03:32 +0000

    create a test file.

--- LFS/src/Inode.py
+++ LFS/src/Inode.py
@@ -4,7 +4,8 @@ import Segment
 import InodeMap
 
 from InodeMap import InodeMapClass
-from Constants import BLOCKSIZE
+from Constants import BLOCKSIZE, INVALIDBLOCKADDRESS
+from FSE import FileSystemException
 
 NUMDIRECTBLOCKS=100 # can have as many as 252 and still fit an Inode in a 1024 byte block
 inodeidpool=1  # 1 is reserved for the root inode
@@ -62,16 +63,40 @@ class Inode:
             # place this block in one of the direct data blocks  
             self.fileblocks[blockoffset] = blockaddress
         else:
-            # XXX - do this after the meteor shower!
-            pass
+            # place this block in an indirect block.
+            blockoffset -= len(self.fileblocks)
+            if self.indirectblock == 0:
+                datablock = Segment.segmentmanager.write_to_newblock(struct.pack("I", blockaddress))
+                self.indirectblock = datablock
+            else :
+                if blockoffset >= (BLOCKSIZE / 4):
+                    raise FileSystemException("File is full.")
+                 
+                olddata = Segment.segmentmanager.blockread(self.indirectblock)
+                # number of pointers to datablocks in indirect block
+                numentries = len(olddata) / 4
+                if blockoffset < numentries:
+                    newdata = olddata[0:blockoffset*4] + struct.pack("I", blockaddress) + olddata[(blockoffset+1)*4:]
+                else:
+                    newdata = olddata + struct.pack("I", INVALIDBLOCKADDRESS)*(numentries - blockoffset - 1) + + struct.pack("I", blockaddress)
+                datablock = Segment.segmentmanager.write_to_newblock(newdata)
+                self.indirectblock = datablock
 
     def _datablockexists(self, blockoffset):
         if blockoffset < len(self.fileblocks):
             return self.fileblocks[blockoffset] != 0
         else:
-            # XXX - do this after the meteor shower!
-            pass
-
+            # check in indirect block.
+            blockoffset -= len(self.fileblocks)
+            if blockoffset >= (BLOCKSIZE / 4):
+                raise FileSystemException("File is full.")
+            indirectblockdata = Segment.segmentmanager.blockread(self.indirectblock)
+            if blockoffset >= (len(indirectblockdata) / 4) :
+                return False
+            blockaddress = struct.unpack("I", indirectblockdata[blockoffset*4 : (blockoffset*4) + 4])[0]
+            return blockaddress != INVALIDBLOCKADDRESS
+                
+            
     # given the number of a data block inside a file, i.e. 0 for
     # the first block, 1 for the second and so forth, returns
     # the contents of that block as a string
@@ -79,8 +104,17 @@ class Inode:
         if blockoffset < len(self.fileblocks):
             blockid = self.fileblocks[blockoffset]
         else:
-            # XXX - do this after the meteor shower!
-            pass
+            # check in indirect block.
+            blockoffset -= len(self.fileblocks)
+            if blockoffset >= (BLOCKSIZE / 4):
+                raise FileSystemException("File is full.")
+            indirectblockdata = Segment.segmentmanager.blockread(self.indirectblock)
+            if blockoffset >= (len(indirectblockdata) / 4) :
+                raise FileSystemException("Block is empty")
+            blockid = struct.unpack("I", indirectblockdata[blockoffset*4 : (blockoffset*4) + 4])[0]
+            if blockid == INVALIDBLOCKADDRESS:
+                raise FileSystemException("Block is empty")
+            
         return Segment.segmentmanager.blockread(blockid)
    
     # perform a read of the file/directory pointed to by 
