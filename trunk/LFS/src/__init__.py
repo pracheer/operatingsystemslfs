ea0dbd533c34be0bab4574bc657c11ba22cfc2db
pracheer.gupta@gmail.com <pracheer.gupta@gmail.com@29f8e827-c577-5851-08f9-08740c10fdc4>
2010-12-06 15:03:32 +0000

    create a test file.

---
+++ trunk/LFS/src/__init__.py
@@ -0,0 +1,15 @@
+from Shell import Shell
+
+shell = Shell()
+
+def runcommand(command):
+    args = command.split(" ")
+    func = getattr(shell, args[0])
+    func(args)
+    
+if __name__ == "__main__":
+    long_string = 'a'*102400  + 'a'
+    runcommand('mkfs -reuse')
+    runcommand('create longfile.txt 102500')
+    runcommand('sync')
+    
